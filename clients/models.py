7c9e9df92cb0500b7145d370dacad49d176d00c1
andrew <j_k7@bk.ru>
2023-04-17 11:09:57 +0300

    modify

---
+++ clients/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Clients(models.Model):
+    phone = models.CharField('Номер телефона', max_length=20)
+    name = models.CharField('ФИО клиента', max_length=255)
+    email = models.EmailField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+    def __str__(self):
+        return self.name + ' ; ' + self.phone
--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'orders',
     'busy_dates',
     'analytical',
+    'clients'
 ]
 
 MIDDLEWARE = [
